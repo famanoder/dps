af7b42af4d83d858f7352f25704de014ab3df1e6
famanoder <915905174@qq.com>
2019-03-12 16:32:12 +0800

    chore: add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: ['./node_modules/@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [2, 'always', [
+        "feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"
+      ]],
+      'subject-full-stop': [0, 'never'],
+      'subject-case': [0, 'never']
+    }
+  };
+  
