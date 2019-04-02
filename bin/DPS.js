4ba2a0bb0e8b7c0efdb21f8623a2dc9c9a03c410
famanoder <915905174@qq.com>
2019-04-02 14:25:21 +0800

    fix: use LF instead of CRLF

--- bin/DPS.js
+++ bin/DPS.js
@@ -9,88 +9,88 @@ const defConf = require('./default.config')
 const DrawPageStructure = require('../src')
 const utils = require('../src/utils')
 
-    program
-    .version(pkg.version)
-    .usage('<command> [options]')
-    .option('-v, --version', 'latest version');
+  program
+  .version(pkg.version)
+  .usage('<command> [options]')
+  .option('-v, --version', 'latest version');
 
-    program
-    .command('init')
-    .description('create a default dps.config.js file')
-    .action(function(env, options) {
-        const dpsConfFile = path.resolve(process.cwd(), defConf.filename)
-        if(fs.existsSync(dpsConfFile)) {
-            return console.log(`\n[${defConf.filename}] had been created! you can edit it and then run 'dps start'\n`)
+  program
+  .command('init')
+  .description('create a default dps.config.js file')
+  .action(function(env, options) {
+    const dpsConfFile = path.resolve(process.cwd(), defConf.filename)
+    if(fs.existsSync(dpsConfFile)) {
+      return console.log(`\n[${defConf.filename}] had been created! you can edit it and then run 'dps start'\n`)
+    }
+    askForConfig().then(ans => {
+      const outputPath = path.resolve(process.cwd(), ans.filepath).replace(/\\/g, '\\\\')
+      prompts({
+        type: 'toggle',
+        name: 'value',
+        message: `Are you sure to create skeleton screen base on ${ans.url}. \n and will output to ${utils.calcText(outputPath)}`,
+        initial: true,
+        active: 'Yes',
+        inactive: 'no'
+      }).then(res => {
+        if(res.value) {
+          fs.writeFile(
+            path.resolve(process.cwd(), defConf.filename), 
+            defConf.getTemplate({
+              url: ans.url,
+              filepath: outputPath
+            }),
+            err => {
+              if(err) throw err;
+              console.log(`\n[${defConf.filename}] had been created! now, you can edit it and then run 'dps start'\n`)
+            }
+          )
         }
-        askForConfig().then(ans => {
-            const outputPath = path.resolve(process.cwd(), ans.filepath).replace(/\\/g, '\\\\')
-            prompts({
-                type: 'toggle',
-                name: 'value',
-                message: `Are you sure to create skeleton screen base on ${ans.url}. \n and will output to ${utils.calcText(outputPath)}`,
-                initial: true,
-                active: 'Yes',
-                inactive: 'no'
-            }).then(res => {
-                if(res.value) {
-                    fs.writeFile(
-                        path.resolve(process.cwd(), defConf.filename), 
-                        defConf.getTemplate({
-                            url: ans.url,
-                            filepath: outputPath
-                        }),
-                        err => {
-                            if(err) throw err;
-                            console.log(`\n[${defConf.filename}] had been created! now, you can edit it and then run 'dps start'\n`)
-                        }
-                    )
-                }
-            })
-        });
+      })
     });
+  });
 
-    program
-    .command('start')
-    .description('start create a skeleton screen')
-    .action(function() {
-        const dpsConfFile = path.resolve(process.cwd(), defConf.filename)
-        if(!fs.existsSync(dpsConfFile)) {
-            utils.log.error(`please run 'dps init' to initialize a config file`, 1)
-        }
-        new DrawPageStructure(require(dpsConfFile)).start()
-    });
+  program
+  .command('start')
+  .description('start create a skeleton screen')
+  .action(function() {
+    const dpsConfFile = path.resolve(process.cwd(), defConf.filename)
+    if(!fs.existsSync(dpsConfFile)) {
+      utils.log.error(`please run 'dps init' to initialize a config file`, 1)
+    }
+    new DrawPageStructure(require(dpsConfFile)).start()
+  });
 
-    program.parse(process.argv);
-    if (program.args.length < 1) program.help()
+  program.parse(process.argv);
+  if (program.args.length < 1) program.help()
 
 function askForConfig() {
-    const questions = [
-        {
-            type: 'text',
-            name: 'url',
-            message: "What's your page url ?",
-            validate: function(value) {
-                const urlReg = /^https?:\/\/.+/ig;
-                if (urlReg.test(value)) {
-                  return true;
-                }
-          
-                return 'Please enter a valid url';
-            }
-        },
-        {
-            type: 'text',
-            name: 'filepath',
-            message: "Enter a relative output filepath ?",
-            validate: function(value) {
-                if(!value) {
-                    return 'Please enter a filepath'
-                }else{
-                    return true;
-                }
-            }
+  const questions = [
+    {
+      type: 'text',
+      name: 'url',
+      message: "What's your page url ?",
+      validate: function(value) {
+        const urlReg = /^https?:\/\/.+/ig;
+        if (urlReg.test(value)) {
+          return true;
         }
-    ];
-    return prompts(questions);
+  
+        return 'Please enter a valid url';
+      }
+    },
+    {
+      type: 'text',
+      name: 'filepath',
+      message: "Enter a relative output filepath ?",
+      validate: function(value) {
+        if(!value) {
+          return 'Please enter a filepath'
+        }else{
+          return true;
+        }
+      }
+    }
+  ];
+  return prompts(questions);
 }
 
